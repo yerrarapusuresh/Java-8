66ce252c09a6f6176100a2174e7cd48b4b4dce10
LEVADATA\syerrarapu <sureshyer@levadata.com>
2018-09-20 23:17:57 +0530

    initial commit

---
+++ Java_8/src/com/suresh/java8/intro/WelcomeInJava8.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.suresh.java8.intro;
+
+/**
+ * @author syerrarapu
+ *
+ */
+public class WelcomeInJava8 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, world");
+	}
+
+}
